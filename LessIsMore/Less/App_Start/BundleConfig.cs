c47d538793446c46a9fd4e2b78fd74f43fbf7a40
Andrew Bater <andrewbater@googlemail.com>
2014-08-01 19:47:35 +0100

    Switch bootstrap to less

--- LessIsMore/Less/App_Start/BundleConfig.cs
+++ LessIsMore/Less/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace Less
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/site.css"));
         }
     }
 }
